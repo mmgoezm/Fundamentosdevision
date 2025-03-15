b3bb21f033dd413ab550e519689ecc5cb2704205
mmgoezm <123196739+mmgoezm@users.noreply.github.com>
2025-03-15 08:30:11 -0500

    Add files via upload

--- Clase 1/Image.py
+++ Clase 1/Image.py
@@ -14,11 +14,11 @@ plt.imshow(Image, interpolation ='none', aspect = 'auto', cmap='gray')
 for (j,i),label in np.ndenumerate(Image):
     plt.text(i,j,label,ha='center',va='center',c='g')
 Image[0,1]=55
-Image[0,2]=155
-Image[0,5]=120
-Image[5,0]=220
-Image[5,0]=90
-Image[5,5]=255
+Image[0,2]=56
+Image[0,5]=57
+Image[5,0]=58
+Image[5,0]=59
+Image[5,5]=60
 Image[6,6]=100
 Image[7,7]=200
 Image[8,14]=180
--- Clase 2/Smoothing.py
+++ Clase 2/Smoothing.py
@@ -10,12 +10,11 @@ Kmean= np.array([
   [1/9, 1/9, 1/9]
 ])
 
-Ksovel= np.array([
+Ksobel= np.array([
   [-1, 0, 1],
   [-2, 0, 2],
   [-1, 0, 1]
 ])
-
 
 kemboss = np.array([
   [-2, -1, 0],
@@ -23,17 +22,15 @@ kemboss = np.array([
   [0, 1, 2]
 ])
 
-
-
-img1 = cv2.filter2D(img,-1,Ksovel)
+img1 = cv2.filter2D(img,-1,Ksobel)
 img2 = cv2.filter2D(img,-1,kemboss)
-img3 = cv2.medianBlur (img,5)
+img3 = cv2.medianBlur (img,9)
 img4 = cv2.bilateralFilter (img,9,75,75)
 
 plt.subplot(1,5,1),  plt.imshow(img), plt.title("Original")
-plt.subplot(1,5,2),  plt.imshow(img1), plt.title("mean (3x3)")
-plt.subplot(1,5,3),  plt.imshow(img2), plt.title("Blurring (4x4)")
-plt.subplot(1,5,4),  plt.imshow(img3), plt.title("Blurring (5x5)")
-plt.subplot(1,5,5),  plt.imshow(img4), plt.title("Blurring (6x6)")
+plt.subplot(1,5,2),  plt.imshow(img1), plt.title("Sobel (3x3)")
+plt.subplot(1,5,3),  plt.imshow(img2), plt.title("Emboss (3x3)")
+plt.subplot(1,5,4),  plt.imshow(img3), plt.title("Medianblur (3x3)")
+plt.subplot(1,5,5),  plt.imshow(img4), plt.title("Bilateral (3x3)")
 
 plt.show()
